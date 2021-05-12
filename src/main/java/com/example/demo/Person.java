1370475809b589d0e4daadd9af69ed7502af5787
Hosni Bey <beyhosni@gmail.com>
2021-05-12 16:05:21 +0200

    Accessing-Data-in-Pivotal-GemFire

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.gemfire.mapping.annotation.Region;
+import lombok.Getter;
+
+
+@Region(value = "People")
+public class Person implements Serializable {
+
+    public String getName() {
+        return name;
+    }
+
+    @Id
+    private final String name;
+
+
+    public int getAge() {
+        return age;
+    }
+
+    private final int age;
+
+    @PersistenceConstructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s is %d years old", getName(), getAge());
+    }
+}
